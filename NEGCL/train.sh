2ea9e938bc6f5178f8454bd0bb9625c014c994db
HubuKG <hubu.csi.edu@gmail.com>
2024-10-17 18:50:19 +0800

    Update train.sh

--- NEGCL/train.sh
+++ NEGCL/train.sh
@@ -1,3 +1,3 @@
-#python main.py -m SRGFormer -d baby -g 0
-#python main.py -m SRGFormer -d sports -g 0
-python main.py -m SRGFormer -d clothing -g 0
+#python main.py -m NEGCL -d baby -g 0
+#python main.py -m NEGCL -d sports -g 0
+python main.py -m NEGCL -d clothing -g 0
